fcec5e06fe26358103f422500283f7997872c23d
Manuel Marín <birrein@LIVE.COM>
2017-08-31 00:58:37 -0300

    Tarea 1 de Lab 06

--- PhoneApp/ViewController.cs
+++ PhoneApp/ViewController.cs
@@ -19,6 +19,8 @@ namespace PhoneApp
 
             TranslateButton.TouchUpInside += (object sender, EventArgs e) =>
             {
+                PhoneNumberText.ResignFirstResponder();
+
                 var Translator = new PhoneTranslator();
                 TranslatedNumber = Translator.ToNumber(PhoneNumberText.Text);
                 if (string.IsNullOrWhiteSpace(TranslatedNumber))
