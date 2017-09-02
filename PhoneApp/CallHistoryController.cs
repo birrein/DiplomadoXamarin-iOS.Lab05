c1682dbc72fb59d186acd3dd809b376ed1fcd0bf
Manuel Marín <birrein@LIVE.COM>
2017-09-02 17:05:05 -0300

    Terminada tarea 2 del Lab 6.

--- PhoneApp/CallHistoryController.cs
+++ PhoneApp/CallHistoryController.cs
@@ -1,13 +1,48 @@
 ﻿using Foundation;
 using System;
+using System.Collections.Generic;
 using UIKit;
 
 namespace PhoneApp
 {
     public partial class CallHistoryController : UITableViewController
     {
+        public List<string> PhoneNumbers { get; set; }
+        protected NSString CallHistoryCellID = new NSString("CallHistoryCell");
+
         public CallHistoryController (IntPtr handle) : base (handle)
         {
+            TableView.RegisterClassForCellReuse(typeof(UITableViewCell), CallHistoryCellID);
+            TableView.Source = new CallHistoryDataSource(this);
+        }
+
+        class CallHistoryDataSource : UITableViewSource
+        {
+            CallHistoryController Controller;
+
+            public CallHistoryDataSource(CallHistoryController controller)
+            {
+                // Almacenar la instancia del UITableViewController.
+                this.Controller = controller;
+            }
+
+            public override nint RowsInSection(UITableView tableview, nint section)
+            {
+                return Controller.PhoneNumbers.Count;
+            }
+
+            public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+            {
+                // Devuelve un UITableViewCell reutilizable creado con el identificador
+                // Controller.CallHistoryCellID
+                var Cell = tableView.DequeueReusableCell(Controller.CallHistoryCellID);
+
+                // Asignar el contenido a mostrar en la celda
+                Cell.TextLabel.Text = Controller.PhoneNumbers[indexPath.Row];
+
+                // Devolver la celda
+                return Cell;
+            }
         }
     }
 }
